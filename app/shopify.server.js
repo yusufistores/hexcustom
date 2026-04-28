5c4b7b159d2b0266f65485b2e7c03fb4488c8996
yusufistores <shop.yusufistores@gmail.com>
2026-04-28 11:22:44 +0530

    Update shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -15,7 +15,7 @@ const shopify = shopifyApp({
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
-  distribution: AppDistribution.AppStore,
+  distribution: AppDistribution.Custom,
   future: {
     expiringOfflineAccessTokens: true,
   },
