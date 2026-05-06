5a34afe4c18bbb77d0f33874609203a9477918bc
yusufistores <shop.yusufistores@gmail.com>
2026-05-06 23:29:01 +0530

    Add files via upload

---
+++ app/routes/session-token.jsx
@@ -0,0 +1,11 @@
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+  return null;
+};
+
+export default function SessionToken() {
+  return null;
+}
+
